c31112ec6c2e5d0f5c06030b26e72e9d19fdb06e
Jamison Holt <JamisonHolt95@gmail.com>
2018-02-07 19:06:02 -0600

    Added animated chart UI

--- Mobile/App.js
+++ Mobile/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import RawChart from './components/chart';
+// import Test from './components/test';
 
 export default class App extends React.Component {
   render() {
--- Mobile/components/chart.js
+++ Mobile/components/chart.js
@@ -1,27 +1,92 @@
 import React from 'react';
 import { StyleSheet, Text, View, Component } from 'react-native';
-import PureChart from 'react-native-pure-chart';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white',
-  },
-  chart: {
-    width: 10,
-    height: 10,
-  },
-});
+import ChartView from 'react-native-highcharts';
 
 export default class RawChart extends React.Component {
   render() {
-    let sampleData = [30, 200, 170, 250, 10];
-    return(
-      <View style={styles.container}>
-        <PureChart style={styles.chart} data={sampleData} type='line' />
-      </View>
+    const Highcharts='Highcharts';
+    const conf={
+      chart: {
+        type: 'spline',
+        animation: Highcharts.svg, // don't animate in old IE
+        marginRight: 10,
+        events: {
+          load: function () {
+
+            // set up the updating of the chart each second
+            let series = this.series[0];
+            setInterval(() => {
+              let x = (new Date()).getTime(); // current time
+              let y = Math.random();
+              series.addPoint([x, y], true, true);
+            }, 2000);
+          }
+        }
+      },
+      title: {
+        text: 'Live random data'
+      },
+      xAxis: {
+        type: 'datetime',
+        tickPixelInterval: 150
+      },
+      yAxis: {
+        title: {
+          text: 'Value'
+        },
+        plotLines: [{
+          value: 0,
+          width: 1,
+          color: '#808080'
+        }]
+      },
+      tooltip: {
+        formatter: function () {
+          return '<b>' + this.series.name + '</b><br/>' +
+          Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
+          Highcharts.numberFormat(this.y, 2);
+        }
+      },
+      legend: {
+        enabled: false
+      },
+      exporting: {
+        enabled: false
+      },
+      series: [{
+        name: 'Random data',
+        data: (function () {
+          // generate an array of random data
+          let data = [],
+          time = (new Date()).getTime(),
+          i;
+
+          for (i = -19; i <= 0; i += 1) {
+            data.push({
+              x: time + i * 1000,
+              y: Math.random()
+            });
+          }
+          return data;
+        }())
+      }],
+      credits: {
+        enabled: false
+      }
+    };
+
+    const options = {
+      global: {
+        useUTC: false
+      },
+      lang: {
+        decimalPoint: ',',
+        thousandsSep: '.'
+      }
+    };
+
+    return (
+      <ChartView style={{height:300, width:350}} config={conf} options={options}></ChartView>
     );
   }
 }
