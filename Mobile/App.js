41fc67b603b7386f0b7bd9b5276b1c1460ecf2b6
JamisonHolt <JamisonHolt95@gmail.com>
2018-04-13 03:16:43 -0500

    Saving bc I want to redo my entire UI whoo

--- Mobile/App.js
+++ Mobile/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Dimensions, Text, View, Button } from 'react-native';
+import { StyleSheet, Dimensions, Text, View, Button, Picker } from 'react-native';
 
 import TempDisplay from './components/tempdisplay';
 
@@ -9,7 +9,8 @@ export default class App extends React.Component {
     super();
     this.state = {
       isCelsius: false,
-      isPortrait: this.updateIsPortrait()
+      isPortrait: this.updateIsPortrait(),
+      selectedDevice: 'Select a device'
     }
 
     Dimensions.addEventListener('change', () => {
@@ -28,18 +29,42 @@ export default class App extends React.Component {
     const styles = this.state.isPortrait ? portraitStyles : landscapeStyles;
     return (
       <View style={styles.background}>
-        <Button
-          title="Button"
-          style={styles.button}
-          onPress={() => {this.setState({
-            isCelsius: !(this.state.isCelsius)
-          });
-        }}
-        />
         <TempDisplay
           style={styles.tempDisplay}
           isPortrait={ this.state.isPortrait }
         />
+        <View style={styles.button} >
+          <Button
+            title="Change to Celsius"
+            color="#BF5700"
+            style={styles.button}
+            onPress={() => {
+              this.setState({
+                isCelsius: !(this.state.isCelsius)
+              });
+            }}
+          />
+          <View style={styles.bluetooth} >
+            <View style={styles.blueToothDropdown} >
+              <Picker 
+                selectedValue={this.state.selectedDevice}
+                style={{ backgroundColor: "#BF5700"}}
+                itemStyle={{ color: '#BF5700'}}
+                onValueChange={(itemValue, itemIndex) => this.setState({selectedDevice: itemValue})}
+                >
+                <Picker.Item label='Select a device' value={null} />                
+                <Picker.Item label='00:21:13:01:1C:51' value='00:21:13:01:1C:51' />
+              </Picker>
+            </View>
+            <View style={styles.blueToothButton} >
+              <Button
+                title="Connect"
+                color="#BF5700"           
+                onPress={() => {}}   
+              />
+            </View>
+          </View>
+        </View>
       </View>
     );
   }
@@ -52,10 +77,23 @@ const portraitStyles = StyleSheet.create({
     backgroundColor: '#333F48',
     justifyContent: 'center'
   },
+  bluetooth: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  blueToothDropdown: {
+    flex: 2,
+    marginRight: 5
+  },
+  blueToothButton: {
+    flex: 1,
+  },
   button: {
-    marginTop: 40,
+    margin: 20,
+    marginBottom: 0,
     flex: 3,
-    color: '#BF5700'
   },
   tempDisplay: {
     flex: 10,
@@ -71,9 +109,8 @@ const landscapeStyles = StyleSheet.create({
     backgroundColor: '#333F48',
   },
   button: {
-    marginTop: 40,
+    marginTop: 20,
     flex: 1,
-    color: '#BF5700',
     flexDirection: 'row'
   },
   tempDisplay: {
