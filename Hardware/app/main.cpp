105af43f3841a0c95e3c8b38b751d6415427f707
JamisonHolt <JamisonHolt95@gmail.com>
2018-04-17 17:04:36 -0500

    Updated Hardware to only send data when temperature has changed

--- Hardware/app/main.cpp
+++ Hardware/app/main.cpp
@@ -1,40 +1,45 @@
 #include "application.h"
 #include "Adafruit/src/MCP9808.h"
+
 // Allow offline development by preventing blocking io from WiFi module
 SYSTEM_MODE(MANUAL);
+
 // Create the MCP9808 temperature sensor object
 MCP9808 tempSensor = MCP9808();
+
 // Constants
 const int BAUD_RATE = 9600;
 const int LED = D7;
+
 // Globals
 int LED_State = 1;
-float fahrenheit;
+float prevTemp = 0;
 
+// Setup our device
 void setup() {
-  // Set LED pin to output type
-  pinMode(LED, OUTPUT);
-  // Begin USB serial operations
-  Serial.begin(BAUD_RATE);
-  delay(5000);
-  // Make sure temperature sensor is found
-  Serial.println("Attempting to connect to tempSensor");
-  while (!tempSensor.begin()) {
-	    Serial.println("MCP9808 not found");
-	    delay(500);
-	}
-  // Set resolution to the maximum (slowest conversion)
-	tempSensor.setResolution(MCP9808_SLOWEST);
-	Serial.println("MCP9808 OK");
-  // Begin output serial operations (TX pin)
-  Serial1.begin(BAUD_RATE);
+    // Set LED pin to output type
+    pinMode(LED, OUTPUT);
+
+    // Make sure temperature sensor is found
+    while (!tempSensor.begin()) {
+        delay(500);
+        LED_State ^= 1;
+    }
+
+    // Set resolution to the maximum (slowest conversion)
+    tempSensor.setResolution(MCP9808_SLOWEST);
+
+    // Begin output serial operations (TX pin)
+    Serial1.begin(BAUD_RATE);
 }
 
 void loop() {
-  digitalWrite(LED, LED_State);
-  LED_State ^= 1;
-  fahrenheit = tempSensor.getTemperature() * 9.0 / 5.0 + 32;
-  Serial.println(fahrenheit);
-  Serial1.println(fahrenheit);
-  delay(250);
+    // Only send changes in temperature and poll as quickly as possible
+    float newTemp = tempSensor.getTemperature() * 9.0 / 5.0 + 32;
+    if (newTemp != prevTemp) {
+        LED_State ^= 1;
+        digitalWrite(LED, LED_State);
+        Serial1.println(newTemp);
+        prevTemp = newTemp;
+    }
 }
